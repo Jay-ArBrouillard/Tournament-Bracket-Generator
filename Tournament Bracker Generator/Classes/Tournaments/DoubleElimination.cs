0e7b5306ea4af39307c3575a98054dc4284db151
DrewCzosek <44214520+DrewCzosek@users.noreply.github.com>
2019-10-07 18:49:58 -0500

    Initial Commit

---
+++ Tournament Bracker Generator/Classes/Tournaments/DoubleElimination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBG.Core.Interfaces;
+
+namespace TBG.Core.Classes.Tournaments
+{
+    public class DoubleElimination : ITournament
+    {
+        public string holder;
+    }
+}
