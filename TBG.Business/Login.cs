368c090438ba6ade81574e58812be72f4dda3319
Jay-Ar Brouillard <brouillardjr@gmail.com>
2019-10-16 21:27:01 -0500

    Update Login UI
    New features include:
    1.) Validation from database.
    2.) Pretty new UI
    3.) Register a new user
    4.) Display messages and change colors when user enters invalid info.
    5.) Everytime a user logs in their last_login_time is saved.

--- TBG.Business/Login.cs
+++ TBG.Business/Login.cs
@@ -18,6 +18,12 @@ namespace TBG.Business
             dbConn.Open();
         }
 
+        /// <summary>
+        /// Return true if the passed username and password match a row in the database.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
         public bool Validate(string user, string pass)
         {
             //check user name empty 
@@ -46,6 +52,78 @@ namespace TBG.Business
 
             return false;
 
+        }
+
+        /// <summary>
+        /// Returns true if the username doesn't match a username in the database.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public bool ValidateUserName(string user)
+        {
+            string query = string.Format("SELECT * FROM `Users` WHERE `user_name` LIKE '{0}'", user);
+            //Validate user exists
+            using (MySqlCommand cmd = new MySqlCommand(query, dbConn))
+            {
+                cmd.ExecuteNonQuery();
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Adds a user to Users table in database. Returns true if successful.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="pass"></param>
+        /// <returns></returns>
+        public bool CreateUser(string user, string pass)
+        {
+            string query = string.Format("INSERT INTO `team4`.`Users` (`user_id`, `user_name`, `password`, `active`, `admin`) VALUES (NULL, '{0}', '{1}', '1', '0')", user, pass);
+            using (MySqlCommand cmd = new MySqlCommand(query, dbConn))
+            {
+                int rowsEffected = cmd.ExecuteNonQuery();
+                if (rowsEffected > 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Updates the last_login column for the passed user.
+        /// </summary>
+        /// <param name="user"></param>
+        public void updateLastLogin(string user)
+        {
+            string query = string.Format("SELECT * FROM `Users` WHERE `user_name` LIKE '{0}'", user);
+            //Validate user exists
+            using (MySqlCommand cmd = new MySqlCommand(query, dbConn))
+            {
+                cmd.ExecuteNonQuery();
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Close();
+                        DateTime myDateTime = DateTime.Now;
+                        string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                        string updateQuery = string.Format("UPDATE `team4`.`Users` SET `last_login` = '{0}' WHERE `Users`.`user_name` = '{1}'", sqlFormattedDate, user);
+                        cmd.CommandText = updateQuery;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            
         }
 
         public void Dispose()
--- TBG.UI/MainWindow.xaml.cs
+++ TBG.UI/MainWindow.xaml.cs
@@ -32,17 +32,81 @@ namespace TBG.UI
             business = ApplicationController.getController();
             login = ApplicationController.getLogin();
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
 
-        private void Login_Event_Handler(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Log a user in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Login_Click(object sender, RoutedEventArgs e)
         {
             string user = userNameTextBox.Text;
             string pass = passwordTextBox.Password;
             //check if eligible to be logged in 
             if (login.Validate(user, pass))
             {
-                MessageBox.Show("You are logged in successfully");
-                //Do something
+                displayMessage.Text = String.Empty;
+                SetDisplayColors(new SolidColorBrush(Colors.Green));
+                login.updateLastLogin(user);
+                //Start Application 
+                //TODO////////////////////////////////////////////////////////
             }
+            else
+            {
+                displayMessage.Text = "Invalid username or password";
+                SetDisplayColors(new SolidColorBrush(Colors.Red));
+            }
+        }
+
+        /// <summary>
+        /// Add a user to the database..
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string user = userNameTextBox.Text;
+            string pass = passwordTextBox.Password;
+            if (!login.ValidateUserName(user))
+            {
+                displayMessage.Text = "Username already exists";
+                SetDisplayColors(new SolidColorBrush(Colors.Red));
+            }
+            else
+            {
+                SetDisplayColors(new SolidColorBrush(Colors.Green));
+
+                bool success = login.CreateUser(user, pass);
+                if (success)
+                {
+                    displayMessage.Text = "Created new user " + user;
+                } 
+                else
+                {
+                    displayMessage.Text = "Error creating new user";
+                }
+            }
+
+        }
+
+        private void SetDisplayColors(SolidColorBrush pColor)
+        {
+            userBorder.Background = pColor;
+            passwordBorder.Background = pColor;
+            loginIcon.Foreground = pColor;
+            passwordIcon.Foreground = pColor;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
