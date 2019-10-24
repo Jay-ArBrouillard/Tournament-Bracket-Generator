22afa530f8421b7efccdf0566d6c4a3f8c34e04a
Jay-Ar Brouillard <brouillardjr@gmail.com>
2019-10-24 18:03:08 -0500

    Now TreeView uses data from database

--- TBG.UI/Tournament.xaml.cs
+++ TBG.UI/Tournament.xaml.cs
@@ -19,60 +19,67 @@ namespace TBG.UI
         {
             InitializeComponent();
             source = ApplicationController.GetProvider();
-            //var teams = convertToTeam(source.getAllTeams());
-            List<TeamTreeView> teams = new List<TeamTreeView>();
-
+            List<TeamTreeView> teams = convertToTeam(source.getAllTeams());
+            /*List<TeamTreeView> teams = new List<TeamTreeView>();
+            
             TeamTreeView team1 = new TeamTreeView()
             {
-                Name = "TankTop Team"
+                TeamName = "TankTop Team"
             };
             team1.Members.Add(new TeamMemberTreeview()
             {
-                Name = "Johnny Smith",
-                Age = 21
+                FirstName = "Johnny Smith",
+                LastName = 21
             });
             team1.Members.Add(new TeamMemberTreeview()
             {
-                Name = "Gerald Smith",
-                Age = 31
+                FirstName = "Gerald Smith",
+                LastName = 31
             });
             teams.Add(team1);
             TeamTreeView team2 = new TeamTreeView()
             {
-                Name = "Team4"
+                TeamName = "Team4"
             };
             team2.Members.Add(new TeamMemberTreeview()
             {
-                Name = "Jayar",
-                Age = 50
+                FirstName = "Jayar",
+                LastName = 50
             });
             team2.Members.Add(new TeamMemberTreeview()
             {
-                Name = "Drew",
-                Age = 20
+                FirstName = "Drew",
+                LastName = 20
             });
             teams.Add(team2);
+            */
 
             participantsTreeView.ItemsSource = teams;
         }
 
-        private List<Team> convertToTeam(List<ITeam> list)
+        private List<TeamTreeView> convertToTeam(List<ITeam> list)
         {
-            List<Team> result = new List<Team>();
+            List<TeamTreeView> result = new List<TeamTreeView>();
 
             foreach(ITeam team in list)
             {
-                List<TeamMember> teamMembers = new List<TeamMember>();
+                ObservableCollection<TeamMemberTreeview> teamMembers = new ObservableCollection<TeamMemberTreeview>();
                 int teamId = team.TeamId;
                 List<ITeamMember> iTeamMembers = source.getTeamMembersByTeamId(teamId);
                 foreach (ITeamMember teamMember in iTeamMembers)
                 {
                     IPerson person = source.getPerson(teamMember.PersonId);
-                    teamMembers.Add(new TeamMember(person.FirstName, 0));
+                    teamMembers.Add(new TeamMemberTreeview()
+                    {
+                        FirstName = person.FirstName,
+                        LastName = person.LastName
+                    });
                 }
 
-                Team newTeam = new Team(team.TeamName, teamMembers);
-                result.Add(newTeam);
+                result.Add(new TeamTreeView(){
+                    TeamName = team.TeamName,
+                    Members = teamMembers
+                });
             }
 
             return result;
@@ -100,15 +107,14 @@ namespace TBG.UI
             this.Members = new ObservableCollection<TeamMemberTreeview>();
         }
 
-        public string Name { get; set; }
+        public string TeamName { get; set; }
 
         public ObservableCollection<TeamMemberTreeview> Members { get; set; }
     }
 
     public class TeamMemberTreeview
     {
-        public string Name { get; set; }
-
-        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
